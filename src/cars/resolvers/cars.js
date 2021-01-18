b7072673a1cccd8faae95ad0bccfbacada5c3d0b
mateki0 <mateki0@interia.pl>
2021-01-18 13:37:35 +0100

    added logo and description

--- src/cars/resolvers/cars.js
+++ src/cars/resolvers/cars.js
@@ -2,8 +2,12 @@ const Car = require("../carSchema");
 
 const cars = async (_, {}) => {
   const cars = await Car.find({}, (err, cars) => {
+    if (err) {
+      console.log(err);
+    }
     return cars;
   });
+  console.log(cars);
   return cars;
 };
 module.exports = cars;
